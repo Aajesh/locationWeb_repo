619b583569ad0afaccdffe14203a814b951b77b5
Aajesh <ajeshchoudhary@gmail.com>
2024-06-26 23:44:25 +0530

    update Repository CrudRepository instead of JpaRepository

--- src/main/java/com/aajesh/location/repo/LocationRepository.java
+++ src/main/java/com/aajesh/location/repo/LocationRepository.java
@@ -1,8 +1,9 @@
 package com.aajesh.location.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LocationRepository extends CrudRepository<Location, Integer> {
+public class LocationRepository extends JpaRepository<Location, Integer> {
 }
