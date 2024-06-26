ac974d6fe5c4b134df669ace5caa360a8e4d6895
Aajesh <ajeshchoudhary@gmail.com>
2024-06-26 23:11:53 +0530

    first commit

---
+++ src/main/java/com/aajesh/location/LocationWebRepoApplication.java
@@ -0,0 +1,13 @@
+package com.aajesh.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationWebRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationWebRepoApplication.class, args);
+	}
+
+}
